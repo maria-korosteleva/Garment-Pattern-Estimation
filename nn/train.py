fa5d4ba0e5755e7212a7ec03e6a3b74d369d5f00
Maria Korosteleva <mariako@kaist.ac.kr>
2020-08-10 17:47:33 +0900

    smallfix -- keep inly stats data

--- nn/train.py
+++ nn/train.py
@@ -10,13 +10,13 @@ def get_values_from_args():
     """command line arguments to control the run for running wandb Sweeps!"""
     # https://stackoverflow.com/questions/40001892/reading-named-command-arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mesh_samples', '-m', help='number of samples per mesh ', type=int, default=5000)
+    parser.add_argument('--mesh_samples', '-m', help='number of samples per mesh ', type=int, default=4000)
     parser.add_argument('--pattern_encoding_size', '-pte', help='size of pattern encoding', type=int, default=100)
     parser.add_argument('--pattern_n_layers', '-ptl', help='size of pattern encoding', type=int, default=3)
     parser.add_argument('--panel_encoding_size', '-pe', help='size of panel encoding', type=int, default=35)
     parser.add_argument('--panel_n_layers', '-pl', help='size of pattern encoding', type=int, default=3)
-    parser.add_argument('--r1', '-r1', help='size of second PN++ layer radius', type=float, default=2)
-    parser.add_argument('--r2', '-r2', help='size of first PN++ layer radius', type=float, default=7)
+    parser.add_argument('--r1', '-r1', help='size of second PN++ layer radius', type=float, default=1.5)
+    parser.add_argument('--r2', '-r2', help='size of first PN++ layer radius', type=float, default=5)
 
     args = parser.parse_args()
     print(args)
@@ -45,16 +45,24 @@ def get_data_config(in_config, old_stats=False):
         # get data stats from older runs to save runtime
         # TODO Update after getting run with zeros in mean edge coordinates!!
         old_experiment = WandbRunWrappper(system_info['wandb_username'],
-            project_name='Test-Garments-Reconstruction', 
-            run_name='Pattern3D-mean', run_id='3hd85b5s'
+            project_name='Garments-Reconstruction', 
+            run_name='Pattern3D-capacity', run_id='3rzpdacg'
         )
         # NOTE data stats are ONLY correct for a specific data split, so these two need to go together
         split, _, data_config = old_experiment.data_info()
+        data_config = {
+            'standardize': data_config['standardize']  # the rest of the info is not needed here
+        }
     else: # default split for reproducibility
         split = {'valid_percent': 10, 'test_percent': 10, 'random_seed': 10} 
         data_config = {}
+
+    print(data_config)
     # update with freshly configured values
     data_config.update(in_config)
+
+    print(data_config)
+
     return split, data_config
 
 
@@ -72,7 +80,7 @@ if __name__ == "__main__":
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
     # NOTE this dataset involves point sampling SO data stats from previous runs might not be correct, especially if we change the number of samples
-    split, data_config = get_data_config(in_data_config, old_stats=False)
+    split, data_config = get_data_config(in_data_config, old_stats=True)
     dataset = data.Garment3DPatternDataset(Path(system_info['datasets_path']) / dataset_folder, 
                                             data_config, gt_caching=True, feature_caching=True)
 
@@ -88,7 +96,7 @@ if __name__ == "__main__":
     if hasattr(model, 'config'):
         trainer.update_config(NN=model.config)  # save NN configuration
 
-    trainer.fit(model)
+    trainer.fit(model)  # Magic happens here
 
     # --------------- Final evaluation --------------
     dataset_wrapper = trainer.datawraper
