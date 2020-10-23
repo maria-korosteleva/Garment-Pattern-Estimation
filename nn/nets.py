66b3275fceebf359bed6573823c6fe335999773e
mariako <mariako@kaist.ac.kr>
2020-10-23 14:59:12 +0900

    revert to linear placement prediction

--- nn/nets.py
+++ nn/nets.py
@@ -406,16 +406,8 @@ class GarmentFullPattern3D(BaseModule):
         )
 
         # decoding the panel placement
-        self.rotation_decoder = nn.Sequential(
-            nn.Linear(self.config['panel_encoding_size'], 50), 
-            nn.ReLU(), 
-            nn.Linear(50, rotation_size),
-        )
-        self.translation_decoder = nn.Sequential(
-            nn.Linear(self.config['panel_encoding_size'], 50), 
-            nn.ReLU(), 
-            nn.Linear(50, translation_size),
-        )
+        self.rotation_decoder = nn.Linear(self.config['panel_encoding_size'], rotation_size)
+        self.translation_decoder = nn.Linear(self.config['panel_encoding_size'], translation_size)
 
         # TODO add stitches prediction modules
 
