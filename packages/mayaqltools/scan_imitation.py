c72dd8283e7bdfb55b30ba71c6578ba9d682ce98
Maria Korosteleva <mariako@kaist.ac.kr>
2020-11-24 17:55:03 +0900

    fixed face removal process

--- packages/mayaqltools/scan_imitation.py
+++ packages/mayaqltools/scan_imitation.py
@@ -49,14 +49,12 @@ def remove_invisible(target, camera_surface, obstacles=[]):
     # Follows the idea of self_intersect_3D() checks used in simulation pipeline
     print('Performing scanning imitation on {} from {} with obstacles {}'.format(target, camera_surface, obstacles))
     
-    # get target as OpenMaya object -- though DAG
+    # get mesh objects for all the inputs target as OpenMaya object -- though DAG
     target_mesh, target_dag = get_mesh(target)
-    target_vertices = OpenMaya.MPointArray()
-    target_mesh.getPoints(target_vertices, OpenMaya.MSpace.kWorld)
-
-    # other meshes
     camera_surface_mesh, _ = get_mesh(camera_surface)
     obstacles_meshes = [get_mesh(name)[0] for name in obstacles]
+
+    # TODO get same meshes with flipped normals!
 
     # search for intersections
     target_accelerator = OpenMaya.MMeshIsectAccelParams()  
@@ -65,11 +63,9 @@ def remove_invisible(target, camera_surface, obstacles=[]):
     invisible_counter = 0
     self_intersect = 0
     object_intersect = 0
-    to_delete_list = []
+    to_delete = []
 
     target_face_iterator = OpenMaya.MItMeshPolygon(target_dag)
-    to_delete = OpenMaya.MDGModifier()  # will collect all delete requests and execute at once
-    last_deleted = -1
     while not target_face_iterator.isDone():  # https://stackoverflow.com/questions/40422082/how-to-find-face-neighbours-in-maya
         # midpoint of the current face -- start of all the rays
         face_mean = OpenMaya.MFloatPoint(target_face_iterator.center())
@@ -80,41 +76,38 @@ def remove_invisible(target, camera_surface, obstacles=[]):
         # TODO Send rays in all directions from the currect vertex
         # Random? Uniform? Same for all vertices? 
         # TODO define depth of testing -- length of the vectors
-        rayDir = OpenMaya.MFloatVector(150., 0., 0.)
+        rayDir = OpenMaya.MFloatVector(0., 0., 150.)
 
         # search setup (for all cases)
+        # Cases when face is invisible from camera
         if not test_intersect(camera_surface_mesh, face_mean, rayDir, cam_surface_accelerator):  # no intesection with camera surface
             invisible_counter += 1
-            target_mesh.deleteFace(face_id)
-            last_deleted = face_id
-            to_delete_list.append(face_id)
+            # to_delete.append(face_id)
         if test_intersect(target_mesh, face_mean, rayDir, target_accelerator, at_least_2=False):  # intersects itself
             self_intersect += 1
-            if last_deleted != face_id:
-                target_mesh.deleteFace(face_id)
-                last_deleted = face_id
-                to_delete_list.append(face_id)
+            if len(to_delete) == 0 or to_delete[-1] != face_id:
+                to_delete.append(face_id)
+                print(face_id, face_mean.x, face_mean.y, face_mean.z)
         if any([test_intersect(mesh, face_mean, rayDir, acc) for mesh, acc in zip(obstacles_meshes, obstacles_accs)]):  # intesects any of the obstacles
-            # vertex is invisible from camera
-            # print('Vertex {}: {} is not visible.'.format(vertex_id, raySource))
             object_intersect += 1
-            if last_deleted != face_id:
-                target_mesh.deleteFace(face_id)
-                last_deleted = face_id
-                to_delete_list.append(face_id)
+            if len(to_delete) == 0 or to_delete[-1] != face_id:
+                to_delete.append(face_id)
+                print(face_id, face_mean.x, face_mean.y, face_mean.z)
         
         target_face_iterator.next()  # iterate!
 
     # Remove invisible vertices
     print('{} invisible, {} self-intersects, {} obstacle instersects out of {}'.format(invisible_counter, self_intersect, object_intersect, target_mesh.numPolygons()))
 
-    print('To delete {} : {}'.format(len(to_delete_list), to_delete_list))
+    print('To delete {} : {}'.format(len(to_delete), to_delete))
 
-    # TODO use MDGModifier instead
-    # to_delete.doIt()
-    # for face_id in to_delete:
-    #     target_mesh.deleteFace(face_id)
-    #     print('Removed {}'.format(face_id))
+    # Removing the faces
+    to_delete = sorted(to_delete)  # for simple id adjustment
+    for idx in range(len(to_delete)):
+        target_mesh.deleteFace(to_delete[idx] - idx)  # adjust for face_id shift after removal
+        print('Removed {}'.format(to_delete[idx]))\
+    
+    print('Removal finished')
 
 
 if __name__ == "__main__":
