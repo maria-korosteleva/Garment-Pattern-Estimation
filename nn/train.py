f0b49d5aa9cd85b4b69183edd6ec29d1a942e5a2
Maria Korosteleva <mariako@kaist.ac.kr>
2021-01-20 10:42:22 +0900

    separate runs setup

--- nn/train.py
+++ nn/train.py
@@ -110,7 +110,7 @@ def get_data_config(in_config, old_stats=False):
 
     else:  # default split for reproducibility
         # NOTE addining 'filename' property to the split will force the data to be loaded from that list, instead of being randomly generated
-        split = {'valid_percent': 20, 'test_percent': 20, 'random_seed': 10} 
+        split = {'valid_percent': 20, 'test_percent': 20, 'random_seed': 10, 'filename': './wandb/data_split.json'} 
         data_config = {}
 
     # update with freshly configured values
@@ -128,8 +128,8 @@ if __name__ == "__main__":
     dataset_folder = 'data_1000_tee_200527-14-50-42_regen_200612-16-56-43'
     dataset_list = [
         'data_1000_tee_200527-14-50-42_regen_200612-16-56-43',
-        'data_1000_skirt_4_panels_200616-14-14-40', 
-        'data_1000_pants_straight_sides_210105-10-49-02'
+        # 'data_1000_skirt_4_panels_200616-14-14-40', 
+        # 'data_1000_pants_straight_sides_210105-10-49-02'
     ]
     in_data_config, in_nn_config, net_seed = get_values_from_args()
 
@@ -137,11 +137,11 @@ if __name__ == "__main__":
     experiment = WandbRunWrappper(
         system_info['wandb_username'], 
         project_name='Garments-Reconstruction', 
-        run_name='multi-all-fin', 
+        run_name='multi-tee-fin', 
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
     # NOTE this dataset involves point sampling SO data stats from previous runs might not be correct, especially if we change the number of samples
-    split, data_config = get_data_config(in_data_config, old_stats=True)
+    split, data_config = get_data_config(in_data_config, old_stats=False)
 
     data_config.update(data_folders=dataset_list)
     # dataset = data.Garment2DPatternDataset(Path(system_info['datasets_path']) / dataset_folder, data_config, gt_caching=True, feature_caching=True)
