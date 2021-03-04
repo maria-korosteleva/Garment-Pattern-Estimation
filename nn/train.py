8647fc497ae00940dd18aa8733768147ba345b33
Maria Korosteleva <mariako@kaist.ac.kr>
2021-03-04 13:08:47 +0900

    update reference run

--- nn/train.py
+++ nn/train.py
@@ -91,14 +91,14 @@ def get_data_config(in_config, old_stats=False):
         # get data stats from older runs to save runtime
         old_experiment = WandbRunWrappper(
             system_info['wandb_username'],
-            project_name='Test-Garments-Reconstruction', 
-            run_name='tee-origin-fix', run_id='1igngxg0'
+            project_name='Garments-Reconstruction', 
+            run_name='multi-all-origin-fix', run_id='w9dgekok'
             # run_name='multi-all-split-data-stats', run_id='2m2w6uns'
         )
         # NOTE data stats are ONLY correct for a specific data split, so these two need to go together
         split, _, data_config = old_experiment.data_info()
         data_config = {
-            'standardize': data_config['standardize'],
+            # 'standardize': data_config['standardize'],
             'max_pattern_len': data_config['max_pattern_len'],
             'max_panel_len': data_config['max_panel_len'],
             'max_num_stitches': data_config['max_num_stitches']  # the rest of the info is not needed here
@@ -130,8 +130,8 @@ if __name__ == "__main__":
     # dataset_folder = 'data_1000_skirt_4_panels_200616-14-14-40'
     dataset_folder = 'data_1000_tee_200527-14-50-42_regen_200612-16-56-43'
     dataset_list = [
-        'data_uni_1000_tee_200527-14-50-42_regen_200612-16-56-43',
-        'data_uni_1000_skirt_4_panels_200616-14-14-40', 
+        # 'data_uni_1000_tee_200527-14-50-42_regen_200612-16-56-43',
+        # 'data_uni_1000_skirt_4_panels_200616-14-14-40', 
         'data_uni_1000_pants_straight_sides_210105-10-49-02'
     ]
     in_data_config, in_nn_config, net_seed = get_values_from_args()
@@ -140,11 +140,11 @@ if __name__ == "__main__":
     experiment = WandbRunWrappper(
         system_info['wandb_username'], 
         project_name='Garments-Reconstruction', 
-        run_name='multi-all-origin-fix', 
+        run_name='multi-pants-origin-fix', 
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
     # NOTE this dataset involves point sampling SO data stats from previous runs might not be correct, especially if we change the number of samples
-    split, data_config = get_data_config(in_data_config, old_stats=False)
+    split, data_config = get_data_config(in_data_config, old_stats=True)
 
     data_config.update(data_folders=dataset_list)
     # dataset = data.Garment2DPatternDataset(Path(system_info['datasets_path']) / dataset_folder, data_config, gt_caching=True, feature_caching=True)
