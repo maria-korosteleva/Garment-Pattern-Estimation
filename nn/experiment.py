b198625afbba83a1e27a61ca88467f832b83261d
Maria Korosteleva <mariako@kaist.ac.kr>
2021-04-06 17:43:42 +0900

    smallfix

--- nn/experiment.py
+++ nn/experiment.py
@@ -81,12 +81,12 @@ class WandbRunWrappper(object):
         if self.initialized:
             wb.run.summary[tag] = info
         else:
-            run = self._run_object()
             if isinstance(info, dict):
                 # NOTE Related wandb issue: https://github.com/wandb/client/issues/1934
                 for key in info:
                     self.add_statistic(tag + '.' + key, info[key])
             else:
+                run = self._run_object()
                 run.summary[tag] = info
                 run.summary.update()
 
