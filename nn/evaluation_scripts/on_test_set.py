e137d164450180e13a47261f68f44e4711a6a2d9
Maria Korosteleva <mariako@kaist.ac.kr>
2021-04-12 11:38:21 +0900

    name & device fix

--- nn/evaluation_scripts/on_test_set.py
+++ nn/evaluation_scripts/on_test_set.py
@@ -22,8 +22,8 @@ system_info = customconfig.Properties('./system.json')
 experiment = WandbRunWrappper(
     system_info['wandb_username'],
     project_name='Garments-Reconstruction', 
-    run_name='multi-all-order-fix', 
-    run_id='5cnaly5h')  # finished experiment
+    run_name='pants-panel-disentanglement', 
+    run_id='3txt9wae')  # finished experiment
 
 if not experiment.is_finished():
     print('Warning::Evaluating unfinished experiment')
@@ -43,34 +43,34 @@ print('Batch: {}, Split: {}'.format(batch_size, split))
 datawrapper = data.DatasetWrapper(dataset, known_split=split, batch_size=batch_size)
 
 # ----- Model architecture -----
-model = nets.GarmentFullPattern3D(dataset.config, experiment.NN_config())
+model = nets.GarmentFullPattern3DDisentangle(dataset.config, experiment.NN_config())
 
-model.load_state_dict(experiment.load_best_model()['model_state_dict'])
+model.load_state_dict(experiment.load_best_model(device='cuda:0')['model_state_dict'])
 
 # ------- Evaluate --------
 valid_loss = metrics.eval_metrics(model, datawrapper, 'validation')
 print('Validation metrics: {}'.format(valid_loss))
-valid_breakdown = metrics.eval_metrics(model, datawrapper, 'valid_per_data_folder')
-print('Validation metrics per dataset: {}'.format(valid_breakdown))
+# valid_breakdown = metrics.eval_metrics(model, datawrapper, 'valid_per_data_folder')
+# print('Validation metrics per dataset: {}'.format(valid_breakdown))
 
 test_metrics = metrics.eval_metrics(model, datawrapper, 'test')
 print('Test metrics: {}'.format(test_metrics))
-test_breakdown = metrics.eval_metrics(model, datawrapper, 'test_per_data_folder')
-print('Test metrics per dataset: {}'.format(test_breakdown))
+# test_breakdown = metrics.eval_metrics(model, datawrapper, 'test_per_data_folder')
+# print('Test metrics per dataset: {}'.format(test_breakdown))
 
 # print(dataset[276]['features'])  # first element of validation set
 
-# experiment.add_statistic('valid_full', valid_loss)
+experiment.add_statistic('valid_on_best', valid_loss)
 # experiment.add_statistic('valid', valid_breakdown)
-# experiment.add_statistic('test_full', test_metrics)
+experiment.add_statistic('test_on_best', test_metrics)
 # experiment.add_statistic('test', test_breakdown)
 
 # -------- Predict ---------
 # save prediction for validation to file
 # prediction_path = datawrapper.predict(model, save_to=Path(system_info['output']), sections=['validation', 'test'])
 # print('Saved to {}'.format(prediction_path))
-# # reflect predictions info in expetiment
+# # # reflect predictions info in expetiment
 # experiment.add_statistic('pred_folder', prediction_path.name)
 
-# art_name = 'multi-data-scan' if len(datawrapper.dataset.data_folders) > 1 else datawrapper.dataset.data_folders[0] + '-scan'
+# art_name = 'multi-data' if len(datawrapper.dataset.data_folders) > 1 else datawrapper.dataset.data_folders[0]  # + '-scan'
 # experiment.add_artifact(prediction_path, art_name, 'result')
