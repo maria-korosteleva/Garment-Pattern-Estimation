92043096d3c9a9dd0bbb8da997bcf4e452495551
mariako <mariako@kaist.ac.kr>
2021-04-21 17:23:00 +0900

    batch size increase

--- nn/train.py
+++ nn/train.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     experiment = WandbRunWrappper(
         system_info['wandb_username'], 
         project_name='Garments-Reconstruction', 
-        run_name='Pants-multi-gpu-2-60-batch', 
+        run_name='Pants-multi-gpu-2-100-batch', 
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
     # NOTE this dataset involves point sampling SO data stats from previous runs might not be correct, especially if we change the number of samples
