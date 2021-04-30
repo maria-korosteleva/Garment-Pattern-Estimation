0c1dc906e62c1e4e56f248098dd47b0615260691
mariako <mariako@kaist.ac.kr>
2021-04-30 11:11:26 +0900

    small fixes

--- nn/evaluation_scripts/on_any_datasets.py
+++ nn/evaluation_scripts/on_any_datasets.py
@@ -31,14 +31,15 @@ if not experiment.is_finished():
 
 # -------- data -------
 dataset_list = [
-    'test_150_jacket_hood_sleeveless_210331-11-16-33',
-    'test_150_skirt_waistband_210331-16-05-37', 
-    'test_150_tee_hood_210401-15-25-29',
-    'test_150_jacket_sleeveless_210331-15-54-26',
-    'test_150_dress_210401-17-57-12',
-    'test_150_jumpsuit_210401-16-28-21',
-    # 'test_150_jumpsuit'
-    'test_150_wb_jumpsuit_sleeveless_210404-11-27-30'
+    # 'test_150_jacket_hood_sleeveless_210331-11-16-33',
+    # 'test_150_skirt_waistband_210331-16-05-37', 
+    # 'test_150_tee_hood_210401-15-25-29',
+    # 'test_150_jacket_sleeveless_210331-15-54-26',
+    # 'test_150_dress_210401-17-57-12',
+    # 'test_150_jumpsuit_210401-16-28-21',
+    # # 'test_150_jumpsuit'
+    # 'test_150_wb_jumpsuit_sleeveless_210404-11-27-30'
+    'data_uni_1000_tee_200527-14-50-42_regen_200612-16-56-43'
 ]
 
 # data_config also contains the names of datasets to use
@@ -47,6 +48,9 @@ split, batch_size, data_config = experiment.data_info()  # note that run is not 
 data_config.update({'obj_filetag': 'sim'})  # sim\scan imitation stats
 data_config.update(data_folders=dataset_list)
 data_config.pop('max_num_stitches', None)  # NOTE forces re-evaluation of max pattern sizes (but not standardization stats) 
+data_config.update(max_datapoints_per_type=150)
+
+batch_size = 5
 
 dataset = data.Garment3DPatternFullDataset(
     system_info['datasets_path'], data_config, gt_caching=True, feature_caching=True)
@@ -63,21 +67,21 @@ model.load_state_dict(experiment.load_best_model(device='cuda:0')['model_state_d
 # ------- Evaluate --------
 loss = metrics.eval_metrics(model, datawrapper, 'full')
 print('Full metrics on unseen set: {}'.format(loss))
-breakdown = metrics.eval_metrics(model, datawrapper, 'full_per_data_folder')
-print('Metrics per dataset: {}'.format(breakdown))
+# breakdown = metrics.eval_metrics(model, datawrapper, 'full_per_data_folder')
+# print('Metrics per dataset: {}'.format(breakdown))
 
-# ---------- Log to the experiment -----------
+# # ---------- Log to the experiment -----------
 
-experiment.add_statistic('unseen_full', loss)
-experiment.add_statistic('unseen', breakdown)
-experiment.add_statistic('unseen_folders', dataset_list)
+# experiment.add_statistic('unseen_full', loss)
+# experiment.add_statistic('unseen', breakdown)
+# experiment.add_statistic('unseen_folders', dataset_list)
 
 # -------- Predict ---------
 # save predictions to file
 prediction_path = datawrapper.predict(model, save_to=Path(system_info['output']), sections=['full'])
 print('Saved to {}'.format(prediction_path))
 # # reflect predictions info in expetiment
-experiment.add_statistic('unseen_pred_folder', prediction_path.name)
+# experiment.add_statistic('unseen_pred_folder', prediction_path.name)
 
-art_name = 'multi-data-unseen' if len(datawrapper.dataset.data_folders) > 1 else datawrapper.dataset.data_folders[0] + '-unseen'
-experiment.add_artifact(prediction_path, art_name, 'result')
+# art_name = 'multi-data-unseen' if len(datawrapper.dataset.data_folders) > 1 else datawrapper.dataset.data_folders[0] + '-unseen'
+# experiment.add_artifact(prediction_path, art_name, 'result')
--- nn/evaluation_scripts/on_test_set.py
+++ nn/evaluation_scripts/on_test_set.py
@@ -22,8 +22,8 @@ system_info = customconfig.Properties('./system.json')
 experiment = WandbRunWrappper(
     system_info['wandb_username'],
     project_name='Garments-Reconstruction', 
-    run_name='pants-panel-disentanglement', 
-    run_id='3txt9wae')  # finished experiment
+    run_name='Tee-JS-orderless', 
+    run_id='2vvv3tdo')  # finished experiment
 
 if not experiment.is_finished():
     print('Warning::Evaluating unfinished experiment')
@@ -45,30 +45,33 @@ datawrapper = data.DatasetWrapper(dataset, known_split=split, batch_size=batch_s
 # ----- Model architecture -----
 model = nets.GarmentFullPattern3DDisentangle(dataset.config, experiment.NN_config(), experiment.NN_config()['loss'])
 
+if 'device_ids' in experiment.NN_config():  # model from multi-gpu training case
+    model = nn.DataParallel(model, device_ids=['cuda:0'])
+
 model.load_state_dict(experiment.load_best_model(device='cuda:0')['model_state_dict'])
 
 # ------- Evaluate --------
-valid_loss = metrics.eval_metrics(model, datawrapper, 'validation')
-print('Validation metrics: {}'.format(valid_loss))
+# valid_loss = metrics.eval_metrics(model, datawrapper, 'validation')
+# print('Validation metrics: {}'.format(valid_loss))
 # valid_breakdown = metrics.eval_metrics(model, datawrapper, 'valid_per_data_folder')
 # print('Validation metrics per dataset: {}'.format(valid_breakdown))
 
 test_metrics = metrics.eval_metrics(model, datawrapper, 'test')
-print('Test metrics: {}'.format(test_metrics))
+# print('Test metrics: {}'.format(test_metrics))
 # test_breakdown = metrics.eval_metrics(model, datawrapper, 'test_per_data_folder')
 # print('Test metrics per dataset: {}'.format(test_breakdown))
 
 # print(dataset[276]['features'])  # first element of validation set
 
-experiment.add_statistic('valid_on_best', valid_loss)
+# experiment.add_statistic('valid_on_best', valid_loss)
 # experiment.add_statistic('valid', valid_breakdown)
-experiment.add_statistic('test_on_best', test_metrics)
+# experiment.add_statistic('test_on_best', test_metrics)
 # experiment.add_statistic('test', test_breakdown)
 
 # -------- Predict ---------
 # save prediction for validation to file
-# prediction_path = datawrapper.predict(model, save_to=Path(system_info['output']), sections=['validation', 'test'])
-# print('Saved to {}'.format(prediction_path))
+prediction_path = datawrapper.predict(model, save_to=Path(system_info['output']), sections=['validation', 'test'])
+print('Saved to {}'.format(prediction_path))
 # # # reflect predictions info in expetiment
 # experiment.add_statistic('pred_folder', prediction_path.name)
 
