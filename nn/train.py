25946a8a3eb19991c42337cc166630c3183a2cbf
mariako <mariako@kaist.ac.kr>
2021-05-03 13:33:07 +0900

    on other data

--- nn/train.py
+++ nn/train.py
@@ -141,14 +141,14 @@ if __name__ == "__main__":
         # big datasets
         'data_5000_tee_200924-16-57-59_regen_210327-15-20-23',
         # 'data_5000_skirt_4_panels_201019-12-23-24_regen_210331-16-18-32',
-        'data_uni_1000_pants_straight_sides_210105-10-49-02',
-        # 'merged_skirt_2_panels_700_210407-18-30-56',
-        'merged_jumpsuit_sleeveless_950_210412-15-18-06',
+        # 'data_uni_1000_pants_straight_sides_210105-10-49-02',
+        'merged_skirt_2_panels_700_210407-18-30-56',
+        # 'merged_jumpsuit_sleeveless_950_210412-15-18-06',
         # 'merged_skirt_8_panels_950_210412-16-11-33',
         # 'merged_wb_pants_straight_1150_210421-10-50-34',
         'merged_tee_sleeveless_1150_210420-17-50-25',
         # 'merged_jacket_1550_210420-16-54-04',
-        # 'merged_dress_sleeveless_1350_210422-11-26-50',
+        'merged_dress_sleeveless_1350_210422-11-26-50',
         # 'merged_wb_dress_sleeveless_1350_210423-13-14-22',
         # 'merged_jacket_hood_1700_210425-21-23-1'
     ]
@@ -158,7 +158,7 @@ if __name__ == "__main__":
     experiment = WandbRunWrappper(
         system_info['wandb_username'], 
         project_name='Garments-Reconstruction', 
-        run_name='Tee-JS-orderless-placement-origin-match', 
+        run_name='Tee-dress-orderl-placement-origin', 
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
     # NOTE this dataset involves point sampling SO data stats from previous runs might not be correct, especially if we change the number of samples
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     model = nets.GarmentFullPattern3DDisentangle(dataset.config, in_nn_config, in_loss_config)
 
     # Multi-GPU!!!
-    model = nn.DataParallel(model, device_ids=['cuda:0', 'cuda:1'])  # , 'cuda:2'])
+    model = nn.DataParallel(model, device_ids=['cuda:0', 'cuda:1', 'cuda:2'])
     model.module.config['device_ids'] = model.device_ids
 
     model.module.loss.with_quality_eval = True  # False to save compute time
