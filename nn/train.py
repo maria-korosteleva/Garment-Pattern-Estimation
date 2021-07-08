acf0abfe75079d2bda5cf5417f426a74bf4d6bb9
mariako <mariako@kaist.ac.kr>
2021-07-08 10:34:14 +0900

    inspecting wb case

--- nn/train.py
+++ nn/train.py
@@ -98,8 +98,8 @@ def get_values_from_args():
         'stitch_tags_margin': 0.3,
         'epoch_with_stitches': 1000,  # turn off stitches
         'epoch_with_order_matching': 0,
-        'epoch_with_cluster_checks': 160,
-        'gap_cluster_threshold': 0.5,
+        'epoch_with_cluster_checks': 90,
+        'gap_cluster_threshold': 0.6,
         'cluster_gap_nrefs': 5,
         'att_distribution_saturation': 0.03,
         'att_empty_weight': 10,
@@ -134,7 +134,7 @@ def get_data_config(in_config, old_stats=False):
         split = {'valid_per_type': 150, 'test_per_type': 150, 'random_seed': 10, 'type': 'count'}   # , 'filename': './wandb/data_split.json'} 
         data_config = {
             'max_datapoints_per_type': 800,  # upper limit of how much data to grab from each type
-            'max_pattern_len': 30,  # > then the total number of panel classes
+            'max_pattern_len': 14,  # 30,  # > then the total number of panel classes
             'max_panel_len': 14,  # (jumpsuit front)
             'max_num_stitches': 24  # jumpsuit (with sleeves)
         }  
@@ -151,18 +151,18 @@ if __name__ == "__main__":
     np.set_printoptions(precision=4, suppress=True)  # for readability
 
     dataset_list = [
-        'dress_sleeveless_2550',
-        'jumpsuit_sleeveless_2000',
-        'skirt_8_panels_1000',
+        # 'dress_sleeveless_2550',
+        # 'jumpsuit_sleeveless_2000',
+        # 'skirt_8_panels_1000',
         'wb_pants_straight_1500',
-        'skirt_2_panels_1200',
-        'jacket_2200',
-        'tee_sleeveless_1800',
+        # 'skirt_2_panels_1200',
+        # 'jacket_2200',
+        # 'tee_sleeveless_1800',
         'wb_dress_sleeveless_2600',
-        'jacket_hood_2700',
-        'pants_straight_sides_1000',
-        'tee_2300',
-        'skirt_4_panels_1600'
+        # 'jacket_hood_2700',
+        # 'pants_straight_sides_1000',
+        # 'tee_2300',
+        # 'skirt_4_panels_1600'
     ]
     in_data_config, in_nn_config, in_loss_config, net_seed = get_values_from_args()
 
@@ -170,11 +170,11 @@ if __name__ == "__main__":
     experiment = WandbRunWrappper(
         system_info['wandb_username'], 
         project_name='Garments-Reconstruction', 
-        run_name='Tee-JS-all-90-thresholds', 
+        run_name='WB-order', 
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
     # NOTE this dataset involves point sampling SO data stats from previous runs might not be correct, especially if we change the number of samples
-    split, data_config = get_data_config(in_data_config, old_stats=True)
+    split, data_config = get_data_config(in_data_config, old_stats=False)
 
     data_config.update(data_folders=dataset_list)
     # dataset = data.Garment2DPatternDataset(
