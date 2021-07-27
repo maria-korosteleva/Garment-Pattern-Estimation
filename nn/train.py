0ae42cd1885fba29c8f32a33e2b071e4da5a47e1
mariako <mariako@kaist.ac.kr>
2021-07-27 21:16:27 +0900

    re-start tests after update

--- nn/train.py
+++ nn/train.py
@@ -103,7 +103,7 @@ def get_values_from_args():
         'order_by': 'shape_translation',   # placement, translation, stitches, shape_translation
 
         'cluster_by': 'translation',  # 'panel_encodings', 'order_feature', 'translation'
-        'epoch_with_cluster_checks': 100,
+        'epoch_with_cluster_checks': 0,  # 100,
         'gap_cluster_threshold': 0.0,
         'diff_cluster_threshold': 0.1,  # testing New!!
         'cluster_gap_nrefs': 5,
@@ -178,7 +178,7 @@ if __name__ == "__main__":
     experiment = WandbRunWrappper(
         system_info['wandb_username'], 
         project_name='Garments-Reconstruction',  
-        run_name='WB-cluster-bboxes-memory-100', 
+        run_name='WB-cluster-bboxes-memory-0', 
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
     # NOTE this dataset involves point sampling SO data stats from previous runs might not be correct, especially if we change the number of samples
