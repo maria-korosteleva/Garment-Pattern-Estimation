c9a478b9e3b1d52971185fdc807547f30cca1597
mariako <mariako@kaist.ac.kr>
2021-07-29 23:41:13 +0900

    fix the name

--- nn/train.py
+++ nn/train.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
     experiment = WandbRunWrappper(
         system_info['wandb_username'], 
         project_name='Garments-Reconstruction',  
-        run_name='WB-cluster-bboxes-memory-0=lr', 
+        run_name='WB-cluster-bboxes-memory-0-lr', 
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
     # NOTE this dataset involves point sampling SO data stats from previous runs might not be correct, especially if we change the number of samples
