4e393482a6932627d033c64f4b8832a0596dc6b6
Maria Korosteleva <mariako@kaist.ac.kr>
2022-02-28 14:33:20 +0900

    change the project name

--- nn/train.py
+++ nn/train.py
@@ -197,7 +197,7 @@ if __name__ == "__main__":
     system_info = customconfig.Properties('./system.json')
     experiment = WandbRunWrappper(
         system_info['wandb_username'], 
-        project_name='Test-Garments-Reconstruction', 
+        project_name='Garments-Reconstruction', 
         run_name='MLP-Global-filt-Condenced', 
         run_id=None, no_sync=False)   # set run id to resume unfinished run!
 
